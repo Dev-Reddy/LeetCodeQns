fed164a632902ea2d919afc2a2c211909d8d124c
DEV REDDY <devreddy4444@gmail.com>
2024-02-01 21:06:57 +0530

    Beats 99.91% in runtime

---
+++ FEB2024/01FEB2024/sol.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<vector<int>> divideArray(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n - 2; i += 3)
+        {
+            if (i + 2 < n && nums[i + 2] - nums[i] <= k)
+            {
+                ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
+            }
+            else
+            {
+                return vector<vector<int>>();
+            }
+        }
+        return ans;
+    }
+};
