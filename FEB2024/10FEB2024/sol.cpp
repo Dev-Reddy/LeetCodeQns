fc7358a19f4077dba0d886c9322dfb4ede203ba5
DEV REDDY <devreddy4444@gmail.com>
2024-02-10 22:18:24 +0530

    brute force sol

---
+++ FEB2024/10FEB2024/sol.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    bool isPalindrome(string s, int left, int right)
+    {
+        while (left < right)
+        {
+            if (s[left++] != s[right--])
+                return false;
+        }
+        return true;
+    }
+
+    int countSubstrings(string s)
+    {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (isPalindrome(s, i, j))
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
