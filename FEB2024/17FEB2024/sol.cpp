d424700bed88036b4d86689782df03b365fd2bce
DEV REDDY <devreddy4444@gmail.com>
2024-02-17 22:15:00 +0530

    greedy

---
+++ FEB2024/17FEB2024/sol.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int furthestBuilding(vector<int> &h, int bricks, int ladders)
+    {
+        priority_queue<int> p;
+
+        int i = 0;
+        for (i = 0; i < h.size() - 1; i++)
+        {
+            int diff = h[i + 1] - h[i];
+
+            if (diff <= 0)
+            {
+                continue;
+            }
+
+            bricks -= diff;
+            p.push(diff);
+
+            if (bricks < 0)
+            {
+                bricks += p.top();
+                p.pop();
+                ladders--;
+            }
+
+            if (ladders < 0)
+                break;
+        }
+
+        return i;
+    }
+};
