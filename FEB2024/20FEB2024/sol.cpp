efc784c8fc7b0091c5608d7cfbc2df9f4ceb98e0
DEV REDDY <devreddy4444@gmail.com>
2024-02-20 21:34:06 +0530

    accumulate

---
+++ FEB2024/20FEB2024/sol.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int sumOfNums = accumulate(nums.begin(), nums.end(), 0);
+
+        int sumActual = (nums.size() * (nums.size() + 1)) / 2;
+
+        return sumActual - sumOfNums;
+    }
+};
